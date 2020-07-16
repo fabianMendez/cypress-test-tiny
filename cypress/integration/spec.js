d159ddb6684de4d49763c76169d28ebaf2e70691
Fabian Mendez <32204060+fabianMendez@users.noreply.github.com>
2020-07-16 07:48:59 -0500

    Create main.yml

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,5 +1,10 @@
 /// <reference types="cypress" />
-it('Show ISO-8859 characters correctly', () => {
-	cy.visit('http://localhost:8000/test.html')
-	cy.contains('ñ', { timeout: 0 })
+
+const { chromeWebSecurity } = Cypress.config()
+
+describe('Show ISO-8859 characters correctly', () => {
+  it(`With chromeWebSecurity ${chromeWebSecurity ? 'enabled' : 'disabled'}`, () => {
+    cy.visit('http://localhost:8000/test.html')
+    cy.contains('ñ', { timeout: 0 })
+  })
 })
